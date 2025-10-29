03fcc2c28d63bc711230dc7afc10b643b671480f
JeromeChenEQ <jerome.chen.2024@smu.edu.sg>
2025-10-29 03:32:13 +0000

    working ex2 3 4

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,6 +1,6 @@
 <script setup>
     // Import BlogPost component
-    import blogPost from './subcomponents/BlogPost2.vue'
+    import BlogPost2 from './subcomponents/BlogPost2.vue';
 	import axios from 'axios'
 </script>
 
@@ -35,13 +35,29 @@
         methods: {
             deletePost(id) {
                 // TODO: Complete the delete method
+                axios.get(`{this.baseUrl}/deletePost`, {params: {id: id}}).then(response=> {
+                    this.posts = this.posts.filter(post=>post.id!=id)
+                }).catch(error=>{
+                    console.log(error)
+                })
             }
+        },
+        components: {
+            BlogPost2
         }
     }
 </script>
 
 <template>
    <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
-
+    <blogPost2 v-for="post in posts" 
+    :subject="post.subject"
+    :entry="post.entry"
+    :mood="post.mood"
+    :key="post.id"
+    >
+    <button class="btn btn-primary" @click="deletePost(post.id)">Delete</button>
+    </blogPost2>
+    
 </template>
 
